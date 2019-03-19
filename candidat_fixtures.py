fa80dc762cf84ddf8dc46089168557ee9a0a5e5a
Boisson Charly <charly.boisson@live.fr>
2019-03-19 12:28:29 +0100

    Add candidat fixture

---
+++ candidat_fixtures.py
@@ -0,0 +1,29 @@
+def get_candidats():
+
+	return  [
+        {
+            "nom": "Nom candidat1",
+            "prenom": "Prénom candidat1",
+            "experiences": ['experience1','experience2','experience3']
+        },
+        {
+            "nom": "Nom candidat2",
+            "prenom": "Prénom candidat2",
+            "experiences": ['experience1','experience2','experience3']
+        },
+        {
+            "nom": "Nom candidat3",
+            "prenom": "Prénom candidat3",
+            "experiences": ['experience1','experience2','experience3']
+        },
+        {
+            "nom": "Nom candidat4",
+            "prenom": "Prénom candidat4",
+            "experiences": ['experience1','experience2','experience3']
+        },
+        {
+            "nom": "Nom candidat5",
+            "prenom": "Prénom candidat5",
+            "experiences": ['experience1','experience2','experience3']
+        }
+    ]
