cbd476c4ab97b7371677b2a569f5e58e084bd051
Boisson Charly <charly.boisson@live.fr>
2019-03-19 12:28:04 +0100

    Add entity Experience

---
+++ entity/Experience.py
@@ -0,0 +1,13 @@
+class Experience:
+
+    def __init__(self, domain, years, projects):
+        self.domain = domain
+        self.years = years
+        self.projects = projects
+
+    def serialize:
+        return {
+            'domain': self.domain,
+            'years': self.years,
+            'projects': self.projects,
+        }
