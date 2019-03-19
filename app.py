46a84859235ef2e6968f4b7a4a9b6b20fbdd86ee
Boisson Charly <charly.boisson@live.fr>
2019-03-19 12:28:40 +0100

    update app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import random
+import candidat_fixtures
+
 
 app = Flask(__name__)
 
@@ -15,10 +17,23 @@ def index():
 			"quote": "Get your facts first, then you can distort them as you please."
 		}
 	]
+
 	nr_of_quotes = len(quotes)
 	selected_quote = quotes[random.randint(0, nr_of_quotes - 1)]
 	return jsonify(selected_quote)
 
+@app.route("/candidats")
+def get_candidats():
+
+	candidats = candidat_fixtures.get_candidats()
+	nr_of_candidats = len(candidats)
+	selected_candidat = candidats[random.randint(0, nr_of_candidats - 1)]
+	return jsonify(selected_candidat)
+
+@app.route("/api/candidat/<string:id>", methods=["GET"])
+def candidate_by_id(id):
+	candidat = candidat_fixtures.get_candidat(id)
+	return jsonify(candidat)
 
 if __name__ == "__main__":
 	app.run(debug=True)
