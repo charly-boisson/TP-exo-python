1785737306a8789cc4a276209947092a4e6d7779
Boisson Charly <charly.boisson@live.fr>
2019-03-19 15:19:38 +0100

    update route Candidats

--- routes/Candidats.py
+++ routes/Candidats.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, abort, make_response
 from services.data_provider_service import DataProviderService as candidatServices
 
 generate_candidats = candidatServices(5)
-print(generate_candidats.get_candidates())
 
 def get_candidats():
 	candidats = generate_candidats.get_candidates()
