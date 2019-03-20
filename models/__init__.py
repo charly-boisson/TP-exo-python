cc724cd8976b4b13637bc74333b509830689bf4f
Boisson Charly <charly.boisson@live.fr>
2019-03-20 16:48:50 +0100

    update import models

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
-import Candidate
-import Client
-import Interview
-import Position
-import Recruiter
-import Review
+import Candidate from Candidate
+import Client from Client
+import Interview from Interview
+import Position from Position
+import Recruiter from Recruiter
+import Review from Review
 
 __all__ = [Candidate,Client,Interview,Position,Recruiter,Review]
